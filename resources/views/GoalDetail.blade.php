ec34d43c943f9fbd915221c539b93b4cf13f7cde
James <jamesrferguson101@gmail.com>
2019-06-28 18:21:32 +0100

    Adds modal dialog pop-up on goal deletion.

--- resources/views/GoalDetail.blade.php
+++ resources/views/GoalDetail.blade.php
@@ -10,14 +10,25 @@
     
     <a href="/editgoal/{{$goal->goalid}}" class='btn btn-primary'>Edit</a>
 
-    <hr>
+    <hr>    
+    <a href="/" class='btn btn-secondary'>Back</a>
+    <button type="submit" class="btn btn-danger" data-toggle="modal"  data-backdrop="static" data-keyboard="false" data-target="#deleteGoalModal">Delete</button>
 
-    
-
-    {{ Form::open(['method' => 'POST', 'url' => "/deletegoal/{$goal->goalid}", 'class' => 'pull-right']) }}
-        {{Form::hidden('_method', 'DELETE')}}
-        <a href="/" class='btn btn-secondary pull-right'>Back</a>
-        {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
-    {{ Form::close() }}
-
+    {{-- Data delete Modal Dialog --}}
+    <div class="modal fade" id="deleteGoalModal" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Do you want to delete this goal?</h4>
+                </div>
+                <div class="modal-body">
+                    {{ Form::open(['method' => 'POST', 'url' => "/deletegoal/{$goal->goalid}", 'class' => 'pull-right']) }}
+                        {{Form::hidden('_method', 'DELETE')}}
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
+                    {{ Form::close() }}
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
