15d9c233e604d5f833f035a1e145893373bb8b9c
James <jamesrferguson101@gmail.com>
2019-06-29 17:20:58 +0100

    Revert "Adds modal dialog pop-up on goal deletion."
    This reverts commit ec34d43c943f9fbd915221c539b93b4cf13f7cde.

--- resources/views/GoalDetail.blade.php
+++ resources/views/GoalDetail.blade.php
@@ -10,25 +10,14 @@
     
     <a href="/editgoal/{{$goal->goalid}}" class='btn btn-primary'>Edit</a>
 
-    <hr>    
-    <a href="/" class='btn btn-secondary'>Back</a>
-    <button type="submit" class="btn btn-danger" data-toggle="modal"  data-backdrop="static" data-keyboard="false" data-target="#deleteGoalModal">Delete</button>
+    <hr>
 
-    {{-- Data delete Modal Dialog --}}
-    <div class="modal fade" id="deleteGoalModal" tabindex="-1" role="dialog" aria-hidden="true">
-        <div class="modal-dialog modal-lg">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h4 class="modal-title">Do you want to delete this goal?</h4>
-                </div>
-                <div class="modal-body">
-                    {{ Form::open(['method' => 'POST', 'url' => "/deletegoal/{$goal->goalid}", 'class' => 'pull-right']) }}
-                        {{Form::hidden('_method', 'DELETE')}}
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                        {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
-                    {{ Form::close() }}
-                </div>
-            </div>
-        </div>
-    </div>
+    
+
+    {{ Form::open(['method' => 'POST', 'url' => "/deletegoal/{$goal->goalid}", 'class' => 'pull-right']) }}
+        {{Form::hidden('_method', 'DELETE')}}
+        <a href="/" class='btn btn-secondary pull-right'>Back</a>
+        {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
+    {{ Form::close() }}
+
 @endsection
