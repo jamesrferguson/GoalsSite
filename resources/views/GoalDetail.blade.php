3640d5dbdbba201a70bf032b35e7cc75134ed42a
James <jamesrferguson101@gmail.com>
2019-06-30 10:41:15 +0100

    Revert "Adds cancel modal and displays message when no goals have been added."
    This reverts commit 2842df71352eef84d88491445561fcb40bcb94fc.

--- resources/views/GoalDetail.blade.php
+++ resources/views/GoalDetail.blade.php
@@ -10,34 +10,14 @@
     
     <a href="/editgoal/{{$goal->goalid}}" class='btn btn-primary'>Edit</a>
 
-    <hr>    
-
-    <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#deleteGoalModal">Delete</button>          
-    <a href="/" class='btn btn-secondary pull-right'>Back</a>
-
-    <div class="modal" id="deleteGoalModal" tabindex="-1" role="dialog">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">Delete Goal</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <p>Are you sure you want to delete this goal?</p>
-                </div>
-                <div class="modal-footer">
-                    {{ Form::open(['method' => 'POST', 'url' => "/deletegoal/{$goal->goalid}", 'class' => 'pull-right']) }}
-                        {{Form::hidden('_method', 'DELETE')}}
-                        {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                    {{ Form::close() }}
-                </div>
-            </div>
-        </div>
-    </div>
+    <hr>
 
     
+
+    {{ Form::open(['method' => 'POST', 'url' => "/deletegoal/{$goal->goalid}", 'class' => 'pull-right']) }}
+        {{Form::hidden('_method', 'DELETE')}}
+        <a href="/" class='btn btn-secondary pull-right'>Back</a>
+        {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
+    {{ Form::close() }}
 
 @endsection
