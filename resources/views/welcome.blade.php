e54424c2748192c81e98b66f3b15eb9bb76c3404
James <jamesrferguson101@gmail.com>
2019-06-30 10:48:39 +0100

    Revert "Revert "Adds cancel modal and displays message when no goals have been added.""
    This reverts commit 3640d5dbdbba201a70bf032b35e7cc75134ed42a.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,14 +4,18 @@
     
 @section('content')
     <h2>Goals</h2>
-    <h4>Current Goals:</h4>  
+    
 
+    @if(sizeof($goals) > 0)
     <div class="list-group">
+        <h4>Current Goals:</h4>  
         @foreach ($goals as $goal)
             <a href="/goaldetail/{{$goal->goalid}}" class="list-group-item list-group-item-action">{{ $goal->goalname }}</a>
         @endforeach
     </div>
-    
+    @else
+        <p>You haven't created any goals yet! Get started <a href="{!! url('/create') !!}">here</a>.</p>
+    @endif
 @endsection
 
     
